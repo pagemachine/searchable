1e2812829738fe496bc462afb1d976f0a9ba0e5e
Saskia Schreiber <sschreiber@pagemachine.de>
2016-12-05 10:36:39 +0100

    [TASK] First commit, basic setup

---
+++ ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+  'title' => 'Searchable',
+  'description' => 'Elasticsearch companion API for TYPO3',
+  'category' => 'misc',
+  'author' => 'Saskia Schreiber',
+  'author_email' => 'sschreiber@pagemachine.de',
+  'author_company' => 'PAGEmachine AG',
+  'state' => 'alpha',
+  'version' => '0.0.1',
+  'constraints' => [
+    'depends' => [
+      'typo3' => '6.2.10-7.6.99',
+    ],
+    'conflicts' => [
+    ],
+    'suggests' => [
+    ],
+  ],
+];
