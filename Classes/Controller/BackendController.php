d456cb4d6543f3039dcf406e55b92a9f3ceb8628
Saskia Schreiber <sschreiber@pagemachine.de>
2017-01-24 15:44:44 +0100

    [FEATURE] Extend backend module to enable page indexing and search testing
    Mostly for debugging

--- Classes/Controller/BackendController.php
+++ Classes/Controller/BackendController.php
@@ -1,6 +1,11 @@
 <?php
 namespace PAGEmachine\Searchable\Controller;
 
+
+use PAGEmachine\Searchable\Indexer\PagesIndexer;
+use PAGEmachine\Searchable\Search;
+use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Core\Messaging\AbstractMessage;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
 /*
@@ -9,14 +14,52 @@ use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
 class BackendController extends ActionController {
 
-	/**
-	 * Backend controller overview action to show general information about the elasticsearch instance
-	 * 
-	 * @return void
-	 */
-	public function indexAction() {
+    /**
+     * Backend controller overview action to show general information about the elasticsearch instance
+     * 
+     * @return void
+     */
+    public function startAction() {
 
-	}
+
+    }
+
+    /**
+     * Testing action to index pages
+     * 
+     * @return void
+     */
+    public function indexPagesAction() {
+
+        $indexer = new PagesIndexer("typo3");
+
+        $response = $indexer->run();
+
+        if ($response['errors']) {
+            $this->addFlashMessage("Something went wrong with your request.", "Error", AbstractMessage::ERROR);
+        } else {
+            $this->addFlashMessage("Request took " . $response['took'] . "ms.", "Success");
+        }
+
+        
+        $this->redirect("start");
+
+    }
+
+    /**
+     * Function to run search tests in the backend.
+     * @todo remove this when everything works or extend to a debuggig device
+     * @param string $term
+     * @return void
+     */
+    public function searchAction($term) {
+
+        $result = Search::getInstance()->search($term);
+        
+        $this->view->assign('result', $result);
+
+
+    }
 
 
 }
--- ext_tables.php
+++ ext_tables.php
@@ -13,7 +13,7 @@ if (TYPO3_MODE === 'BE') {
         'searchable',
         '',
         array(
-        	'Backend' => 'index'
+        	'Backend' => 'start, indexPages, search'
         ),
         array(
             'access'    => 'user,group',
