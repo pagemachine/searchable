bd069457d79aa677c414e2136b872a146b745541
Saskia Schreiber <sschreiber@pagemachine.de>
2017-01-31 17:30:49 +0100

    [FEATURE] Add basic command controller for indexing

---
+++ Classes/Command/IndexCommandController.php
@@ -0,0 +1,43 @@
+<?php
+namespace PAGEmachine\Searchable\Command;
+
+use PAGEmachine\Searchable\Service\ExtconfService;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use \TYPO3\CMS\Extbase\Mvc\Controller\CommandController;
+
+/*
+ * This file is part of the PAGEmachine Searchable project.
+ */
+
+class IndexCommandController extends CommandController
+{
+
+    /**
+     * Reset all indices (if necessary) and let all defined indexers run
+     * @return void
+     */
+    public function indexFullCommand() {
+
+        $defaultIndex = ExtconfService::getIndex();
+
+        $indexerNames = ExtconfService::getIndexers();
+
+        foreach ($indexerNames as $className) {
+
+            $indexer = GeneralUtility::makeInstance($className, $defaultIndex);
+
+            $result = $indexer->run();
+
+            if ($result['errors']) {
+
+                $this->outputLine("There was an error running " . $className . ":");
+
+                \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($result['errors'], __METHOD__, 5, true);
+                break;
+            }
+        }
+
+        $this->outputLine("Indexing finished.");
+
+    }
+}
