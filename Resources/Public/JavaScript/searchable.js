dc5f08db65e628679f6b15e78af79b07759ab3aa
Saskia Schreiber <sschreiber@pagemachine.de>
2017-07-19 15:01:04 +0200

    [FEATURE] Use jQuery Typeahead to render Live Search results

--- Resources/Public/JavaScript/searchable.js
+++ Resources/Public/JavaScript/searchable.js
@@ -20,4 +20,59 @@ $(document).ready(function(){
 		},
 		triggerSelectOnValidInput: false
 	});
+
+	$('.js-typeahead').typeahead({
+	    minLength: 1,
+	    offset: true,
+	    hint: false,
+	    dynamic: true,
+	    filter: false,
+	    source: {
+	        hits : {
+	            ajax: {
+	                type: "GET",
+	                url: "/?eID=searchable_search",
+	                data: {
+	                	term: "{{query}}"
+	                },
+	            },
+	        }
+	    },
+	    template: function(query, item){
+
+	    	content = "<span class='searchable-result-title'>{{_source.searchable_meta.linkTitle}}</span>";
+
+	    	if (item.highlight != undefined)
+	    	{
+	    		content += "<div class='result-highlight'>...";
+				for (var i = 0, len = item.highlight.searchable_highlight.length; i < len; i++) {
+					content += item.highlight.searchable_highlight[i];
+					content += "...";
+				}
+				content += "</div>";
+	    	}
+	    	return content;
+
+	    },
+	    emptyTemplate: 'No result for "{{query}}"',
+	    href: "{{_source.searchable_meta.renderedLink}}",
+	    selector: {
+	        container: "searchable-typeahead",
+	        result: "searchable-typeahead-field",
+	        list: "list-group searchable-typeahead-list",
+	        group: "searchable-typeahead-group",
+	        item: "list-group-item searchable-typeahead-item",
+	        empty: "searchable-typeahead-empty",
+	        display: "searchable-typeahead-display",
+	        query: "searchable-typeahead-query",
+	        filter: "searchable-typeahead-filter",
+	        filterButton: "searchable-typeahead-filter-button",
+	        dropdown: "searchable-typeahead-dropdown",
+	        dropdownItem: "searchable-typeahead-dropdown-item",
+	        button: "searchable-typeahead-button",
+	        backdrop: "searchable-typeahead-backdrop",
+	        hint: "searchable-typeahead-hint",
+	        cancelButton: "searchable-typeahead-cancel-button"
+    	},
+	});
 });
