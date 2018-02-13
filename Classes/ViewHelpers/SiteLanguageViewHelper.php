3747b4cbdb9f7649151d6de90ecb0c2ea9e27cb6
Saskia Schreiber <sschreiber@pagemachine.de>
2018-02-13 14:42:37 +0100

    [BUGFIX] Live Search does not take current language into account

---
+++ Classes/ViewHelpers/SiteLanguageViewHelper.php
@@ -0,0 +1,34 @@
+<?php
+namespace PAGEmachine\Searchable\ViewHelpers;
+
+/*
+ * This file is part of the PAGEmachine Searchable project.
+ */
+
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+/**
+ * SiteLanguageViewHelper
+ * Simple ViewHelper to return the current site language
+ */
+class SiteLanguageViewHelper extends AbstractViewHelper
+{
+    /**
+     * @return int
+     */
+    public function render()
+    {
+        if (is_object($this->getTypoScriptFrontendController())) {
+            return $this->getTypoScriptFrontendController()->sys_language_uid;
+        }
+        return 0;
+    }
+
+    /**
+     * @return TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController
+     */
+    public function getTypoScriptFrontendController()
+    {
+        return $GLOBALS['TSFE'];
+    }
+}
