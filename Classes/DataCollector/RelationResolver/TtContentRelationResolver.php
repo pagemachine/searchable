608b26358349429c2a25c1ac739f7c8963f6c16f
Francisco Seipel <fseipel@pagemachine.de>
2018-11-13 12:30:01 +0100

    [TASK] Replace TYPO3_DB with querybuilder in TtContentRelationResolver

--- Classes/DataCollector/RelationResolver/TtContentRelationResolver.php
+++ Classes/DataCollector/RelationResolver/TtContentRelationResolver.php
@@ -4,6 +4,9 @@ namespace PAGEmachine\Searchable\DataCollector\RelationResolver;
 use PAGEmachine\Searchable\DataCollector\DataCollectorInterface;
 use PAGEmachine\Searchable\DataCollector\RelationResolver\RelationResolverInterface;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Database\Query\Restriction\DeletedRestriction;
+use TYPO3\CMS\Core\Database\Query\Restriction\FrontendRestrictionContainer;
 use TYPO3\CMS\Core\SingletonInterface;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Frontend\Page\PageRepository;
@@ -69,8 +72,22 @@ class TtContentRelationResolver implements SingletonInterface, RelationResolverI
      */
     protected function fetchContentUids($pid)
     {
-        $content = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows('uid', 'tt_content', 'pid = ' . $pid . ' AND ' . $GLOBALS['TCA']['tt_content']['ctrl']['languageField'] . ' IN(0,-1)' . $this->pageRepository->enableFields('tt_content') . BackendUtility::deleteClause('tt_content'));
-
-        return $content;
+        if (\TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) < 9000000) {
+            $content = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows('uid', 'tt_content', 'pid = ' . $pid . ' AND ' . $GLOBALS['TCA']['tt_content']['ctrl']['languageField'] . ' IN(0,-1)' . $this->pageRepository->enableFields('tt_content') . BackendUtility::deleteClause('tt_content'));
+            return $content;
+        }else{
+            $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('tt_content');
+            $queryBuilder->getRestrictions()
+               ->removeAll()
+               ->add(GeneralUtility::makeInstance(DeletedRestriction::class));
+            $queryBuilder->setRestrictions(GeneralUtility::makeInstance(FrontendRestrictionContainer::class));
+            $queryBuilder->select('uid')
+            ->from('tt_content')
+            ->where(
+               $queryBuilder->expr()->eq('pid', $queryBuilder->createNamedParameter($pid)),
+               $queryBuilder->expr()->in($GLOBALS['TCA']['tt_content']['ctrl']['languageField'], '0,-1')
+            );
+            return $queryBuilder->execute();
+        }
     }
 }
