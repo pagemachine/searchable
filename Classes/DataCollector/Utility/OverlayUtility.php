ac61f97509703602a2b747918fa8e45f7d0a047b
Saskia Schreiber <sschreiber@pagemachine.de>
2019-04-18 14:00:19 +0200

    [TASK] Drop obsolete field overlay conditions
    This is no longer necessary in TYPO3 8 and above (translated fields always contain the correct values

--- Classes/DataCollector/Utility/OverlayUtility.php
+++ Classes/DataCollector/Utility/OverlayUtility.php
@@ -81,10 +81,7 @@ class OverlayUtility implements SingletonInterface
                 continue;
             }
 
-            //If the FE overlay differs from the raw base record, replace the field with the translated field in the processed record
-            if ($this->shouldFieldBeOverlaid($table, $key, $translationData['defaultLanguageRow'][$key])) {
-                $record[$key] = $translationRecord[$key];
-            }
+            $record[$key] = $translationRecord[$key];
         }
 
         return $record;
@@ -108,44 +105,5 @@ class OverlayUtility implements SingletonInterface
         }
 
         return $rawOverlay;
-    }
-
-
-
-   /**
-     * Determine if a field needs an overlay
-     * Copied from PageRepository
-     *
-     * @todo remove this in V8 (overlay mechanism no longer exists then, the translated record holds the correct merged values from the start)
-     *
-     * @param string $table TCA tablename
-     * @param string $field TCA fieldname
-     * @param mixed $value Current value of the field
-     * @return bool Returns TRUE if a given record field needs to be overlaid
-     */
-    public function shouldFieldBeOverlaid($table, $field, $value)
-    {
-        $l10n_mode = isset($GLOBALS['TCA'][$table]['columns'][$field]['l10n_mode'])
-            ? $GLOBALS['TCA'][$table]['columns'][$field]['l10n_mode']
-            : '';
-
-        $shouldFieldBeOverlaid = true;
-
-        if ($l10n_mode === 'exclude') {
-            $shouldFieldBeOverlaid = false;
-        } elseif ($l10n_mode === 'mergeIfNotBlank') {
-            $checkValue = $value;
-
-            // 0 values are considered blank when coming from a group field
-            if (empty($value) && $GLOBALS['TCA'][$table]['columns'][$field]['config']['type'] === 'group') {
-                $checkValue = '';
-            }
-
-            if ($checkValue === [] || !is_array($checkValue) && trim($checkValue) === '') {
-                $shouldFieldBeOverlaid = false;
-            }
-        }
-
-        return $shouldFieldBeOverlaid;
     }
 }
