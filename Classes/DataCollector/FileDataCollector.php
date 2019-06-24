9ffa9d7f85acfce72a907ae65e7362857b503109
Saskia Schreiber <sschreiber@pagemachine.de>
2019-06-24 17:50:57 +0200

    [BUGFIX] Custom FileDataCollector Query is no longer recognized

--- Classes/DataCollector/FileDataCollector.php
+++ Classes/DataCollector/FileDataCollector.php
@@ -1,6 +1,8 @@
 <?php
 namespace PAGEmachine\Searchable\DataCollector;
 
+use Doctrine\DBAL\Connection;
+
 /*
  * This file is part of the PAGEmachine Searchable project.
  */
@@ -52,12 +54,45 @@ class FileDataCollector extends TcaDataCollector implements DataCollectorInterfa
          * @var array
          */
         $statement = parent::buildUidListQueryParts($additionalWhere, $applyLanguageRestriction);
-
         $statement['from'][] = 'sys_file';
 
-        $statement['where'][] = ' AND sys_file_metadata.file = sys_file.uid';
-        $statement['where'][] = ' AND sys_file.mime_type IN(' . implode(',', $this->config['mimetypes']) . ')';
+        if (!empty($this->config['mimetypes'])) {
+
+            $statement['where'][] = ' AND sys_file_metadata.file = sys_file.uid';
+            $statement['where'][] = ' AND sys_file.mime_type IN(' . implode(',', $this->config['mimetypes']) . ')';
+        }
+
+
 
         return $statement;
+    }
+
+    /**
+     * Returns a QueryBuilder object for the record selection query
+     * Modify this method if you want to apply custom restrictions
+     *
+     * @param  bool $applyLanguageRestriction
+     * @return queryBuilder $subCollector
+     */
+    public function buildUidListQueryBuilder($applyLanguageRestriction = false)
+    {
+        $queryBuilder = parent::buildUidListQueryBuilder($applyLanguageRestriction);
+
+        $queryBuilder->join(
+            'sys_file_metadata',
+            'sys_file',
+            'sys_file',
+            $queryBuilder->expr()->eq('sys_file_metadata.file', $queryBuilder->quoteIdentifier('sys_file.uid'))
+        );
+
+        $queryBuilder->andWhere(
+            $queryBuilder->expr()->in(
+                'sys_file.mime_type',
+                $this->config['mimetypes'],
+                Connection::PARAM_STR_ARRAY
+            )
+        );
+
+        return $queryBuilder;
     }
 }
