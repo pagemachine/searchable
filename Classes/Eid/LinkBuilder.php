3e18d0afdad6154e87f64aa9d4cf2d419f6881d8
Mathias Brodala <mbrodala@pagemachine.de>
2020-09-11 12:55:32 +0200

    [TASK] Drop duplicate TCA init in eID context

--- Classes/Eid/LinkBuilder.php
+++ Classes/Eid/LinkBuilder.php
@@ -23,7 +23,6 @@ class LinkBuilder
 
     public function __construct()
     {
-        \TYPO3\CMS\Frontend\Utility\EidUtility::initTCA();
         TsfeUtility::createTSFE();
         $this->contentObjectRenderer = GeneralUtility::makeInstance(ContentObjectRenderer::class);
     }
