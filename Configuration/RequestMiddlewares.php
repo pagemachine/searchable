64523597dd0d7ea6da266b82164a0ccf68a992ca
Mathias Brodala <mbrodala@pagemachine.de>
2020-09-17 17:51:46 +0200

    [TASK] Update middleware identifier

--- Configuration/RequestMiddlewares.php
+++ Configuration/RequestMiddlewares.php
@@ -2,7 +2,7 @@
 
 return [
     'frontend' => [
-        'fes/api/routing' => [
+        'pagemachine/searchable/uri-builder' => [
             'target' => \PAGEmachine\Searchable\Middleware\UriBuilder::class,
             'before' => [
                 'typo3/cms-frontend/static-route-resolver',
