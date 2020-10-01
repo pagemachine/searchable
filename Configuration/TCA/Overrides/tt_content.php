bf891220b535f549b151b2d96793a1e46040849c
Mathias Brodala <mbrodala@pagemachine.de>
2020-10-01 15:30:21 +0200

    [BUGFIX] Properly register implicit TCA overrides

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -4,8 +4,22 @@ if (!defined('TYPO3_MODE')) {
 }
 
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['searchable_searchbar'] = 'select_key, pages, recursive';
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['searchable_livesearchbar'] = 'select_key, pages, recursive';
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['searchable_results'] = 'select_key, pages, recursive';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['searchable_searchbar'] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    'searchable_searchbar',
+    'FILE:EXT:searchable/Configuration/Flexforms/Searchbar.xml'
+);
+
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['searchable_livesearchbar'] = 'select_key, pages, recursive';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['searchable_livesearchbar'] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    'searchable_livesearchbar',
+    'FILE:EXT:searchable/Configuration/Flexforms/LiveSearchbar.xml'
+);
+
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['searchable_results'] = 'select_key, pages, recursive';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['searchable_results'] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    'searchable_results',
+    'FILE:EXT:searchable/Configuration/Flexforms/Results.xml'
+);
