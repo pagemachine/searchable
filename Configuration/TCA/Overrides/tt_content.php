e3e375ee2f777e0008e2434a168f87f464e90a0f
Mathias Brodala <mbrodala@pagemachine.de>
2020-10-09 15:30:05 +0200

    [TASK] Move plugin registration to TCA overrides

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -3,6 +3,11 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'PAGEmachine.Searchable',
+    'Searchbar',
+    'Searchable: Search bar'
+);
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['searchable_searchbar'] = 'select_key, pages, recursive';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['searchable_searchbar'] = 'pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
@@ -10,6 +15,11 @@ $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['searchable_s
     'FILE:EXT:searchable/Configuration/Flexforms/Searchbar.xml'
 );
 
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'PAGEmachine.Searchable',
+    'LiveSearchbar',
+    'Searchable: Live Search bar (AJAX)'
+);
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['searchable_livesearchbar'] = 'select_key, pages, recursive';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['searchable_livesearchbar'] = 'pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
@@ -17,6 +27,11 @@ $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['searchable_l
     'FILE:EXT:searchable/Configuration/Flexforms/LiveSearchbar.xml'
 );
 
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'PAGEmachine.Searchable',
+    'Results',
+    'Searchable: Results'
+);
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['searchable_results'] = 'select_key, pages, recursive';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['searchable_results'] = 'pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
--- ext_tables.php
+++ ext_tables.php
@@ -3,24 +3,6 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'PAGEmachine.Searchable',
-    'Searchbar',
-    'Searchable: Search bar'
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'PAGEmachine.Searchable',
-    'LiveSearchbar',
-    'Searchable: Live Search bar (AJAX)'
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'PAGEmachine.Searchable',
-    'Results',
-    'Searchable: Results'
-);
-
 // Backend module
 if (TYPO3_MODE === 'BE') {
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
