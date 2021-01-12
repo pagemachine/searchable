a229d2c5a065e4a8992357d1a29aab971d5b004e
Mathias Brodala <mbrodala@pagemachine.de>
2021-01-12 09:20:35 +0100

    [BUGFIX] Fall back to first site
    In case no site could be resolved in the link building request (e.g. if
    a development host is used not covered by site config), we now fall back
    to the first site.
    
    This basically reflects the PseudoSite handling which was dropped with
    TYPO3v10 which was used since we did construct TSFE with a fixed page
    UID "1".

--- Classes/Middleware/UriBuilder.php
+++ Classes/Middleware/UriBuilder.php
@@ -9,6 +9,8 @@ use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use TYPO3\CMS\Core\Context\Context;
 use TYPO3\CMS\Core\Http\JsonResponse;
+use TYPO3\CMS\Core\Site\Entity\NullSite;
+use TYPO3\CMS\Core\Site\SiteFinder;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Utility\VersionNumberUtility;
 use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
@@ -62,11 +64,18 @@ final class UriBuilder implements MiddlewareInterface
             return;
         }
 
+        $site = $request->getAttribute('site');
+
+        if ($site instanceof NullSite) {
+            $siteFinder = GeneralUtility::makeInstance(SiteFinder::class);
+            $site = array_values($siteFinder->getAllSites())[0] ?? $site;
+        }
+
         $frontendController = GeneralUtility::makeInstance(
             TypoScriptFrontendController::class,
             GeneralUtility::makeInstance(Context::class),
-            $request->getAttribute('site'),
-            $request->getAttribute('language'),
+            $site,
+            $site->getDefaultLanguage(),
             $request->getAttribute('routing'),
             $request->getAttribute('frontend.user')
         );
