3b6732e0f6073d9db8d144995067e420c2a86cbb
Francisco Seipel <fseipel@pagemachine.de>
2023-10-30 16:39:46 +0100

    [TASK] Show a message if elasticsearch is slow

---
+++ Classes/Utility/TimeMeasurementUtility.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace PAGEmachine\Searchable\Utility;
+
+use TYPO3\CMS\Core\Messaging\FlashMessage;
+use TYPO3\CMS\Core\Messaging\FlashMessageService;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/*
+ * This file is part of the PAGEmachine Searchable project.
+ */
+
+/**
+ * Helper class to measure time and show an error if necessary
+ */
+class TimeMeasurementUtility
+{
+    /**
+     * @var float
+     */
+    protected $start;
+
+    /**
+     * @var int
+     */
+    protected $totalTime;
+
+    /**
+     * @param int $timeout
+     */
+    protected $timeout;
+
+    public function __construct($timeout = null)
+    {
+        $this->timeout = $timeout;
+        return $this;
+    }
+
+    public function start()
+    {
+        $this->start = microtime(true);
+
+        return $this;
+    }
+
+    public function stop()
+    {
+        $this->totalTime = microtime(true) - $this->start;
+
+        if ($this->timeout && $this->totalTime > $this->timeout) {
+            $this->showError($this->totalTime);
+        }
+    }
+
+    protected function showError($time)
+    {
+        $text = $GLOBALS['LANG']->sL('LLL:EXT:searchable/Resources/Private/Language/locallang_be.xlf:searchable_module_timeout');
+
+        $message = GeneralUtility::makeInstance(
+            FlashMessage::class,
+            $text . ' (' . number_format($time, 2) . 's)',
+            '',
+            FlashMessage::WARNING,
+            true
+        );
+
+        $flashMessageService = GeneralUtility::makeInstance(FlashMessageService::class);
+        $messageQueue = $flashMessageService->getMessageQueueByIdentifier();
+        $messageQueue->addMessage($message);
+    }
+}
