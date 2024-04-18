f54a3b9c290762760d44d5dfc51990340be3afa3
Francisco Seipel <fseipel@pagemachine.de>
2024-04-18 11:56:10 +0200

    [TASK] Change rector config

--- rector.php
+++ rector.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\Php74\Rector\LNumber\AddLiteralSeparatorToNumberRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
-use Rector\Set\ValueObject\LevelSetList;
 use Ssch\TYPO3Rector\Set\Typo3SetList;
 
 return RectorConfig::configure()
@@ -13,13 +12,13 @@ return RectorConfig::configure()
         __DIR__ . '/Classes',
         __DIR__ . '/Tests',
     ])
+    ->withRootFiles()
     ->withSkip([
         __DIR__ . '/Tests/Functional/Fixtures/Extensions/*/Configuration/*',
     ])
     ->withImportNames(true, false, false, true)
     ->withPhpSets()
     ->withSets([
-        LevelSetList::UP_TO_PHP_74,
         PHPUnitSetList::PHPUNIT_90,
         Typo3SetList::TYPO3_11,
     ])
