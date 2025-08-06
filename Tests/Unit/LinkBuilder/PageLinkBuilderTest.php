2c2b1c34db3d0e9a34ee4849b0c6f4596dff7a91
Francisco Seipel <fseipel@pagemachine.de>
2025-08-06 16:25:29 +0200

    [TASK] Use linkFactory instead of using a frontend request

--- Tests/Unit/LinkBuilder/PageLinkBuilderTest.php
+++ Tests/Unit/LinkBuilder/PageLinkBuilderTest.php
@@ -3,8 +3,6 @@ namespace PAGEmachine\Searchable\Tests\Unit\LinkBuilder;
 
 use PAGEmachine\Searchable\LinkBuilder\PageLinkBuilder;
 use Prophecy\PhpUnit\ProphecyTrait;
-use TYPO3\CMS\Core\Http\RequestFactory;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
 
 /*
@@ -22,17 +20,6 @@ class PageLinkBuilderTest extends UnitTestCase
      * @var PageLinkBuilder
      */
     protected $pageLinkBuilder;
-
-    /**
-     * Set up this testcase
-     */
-    public function setUp(): void
-    {
-        parent::setUp();
-
-        $requestFactoryProphecy = $this->prophesize(RequestFactory::class);
-        GeneralUtility::addInstance(RequestFactory::class, $requestFactoryProphecy->reveal());
-    }
 
     /**
      * @test
