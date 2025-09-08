018e87c6c36e1cabbc37d28593fe9ec475fc58f0
Francisco Seipel <fseipel@pagemachine.de>
2025-09-08 14:10:20 +0200

    [TASK] Init writerConfiguration inside of index commands instead of localconf

--- ext_localconf.php
+++ ext_localconf.php
@@ -12,10 +12,8 @@ use PAGEmachine\Searchable\Query\SearchQuery;
 use TYPO3\CMS\Core\Configuration\Exception\ExtensionConfigurationExtensionNotConfiguredException;
 use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 use TYPO3\CMS\Core\Configuration\FlexForm\FlexFormTools;
-use TYPO3\CMS\Core\Core\Environment;
 use TYPO3\CMS\Core\Log\LogLevel;
 use TYPO3\CMS\Core\Log\Writer\FileWriter;
-use TYPO3\CMS\Core\Log\Writer\PhpErrorLogWriter;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
@@ -63,10 +61,6 @@ if (empty($GLOBALS['TYPO3_CONF_VARS']['LOG']['PAGEmachine']['Searchable']['write
             ],
         ],
     ];
-}
-
-if (Environment::isCli()) {
-    $GLOBALS['TYPO3_CONF_VARS']['LOG']['PAGEmachine']['Searchable']['writerConfiguration'][LogLevel::INFO][PhpErrorLogWriter::class] = [];
 }
 
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['searchable'] = [
