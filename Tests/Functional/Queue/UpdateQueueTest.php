e790ad7d87b542fdf45dc41c3d665313e1c79732
Francisco Seipel <fseipel@pagemachine.de>
2026-04-15 13:57:40 +0200

    [TASK] Test if data is inserted correctly in the update queue table

--- Tests/Functional/Queue/UpdateQueueTest.php
+++ Tests/Functional/Queue/UpdateQueueTest.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace PAGEmachine\Searchable\Tests\Functional\Queue;
 
+use Doctrine\DBAL\ParameterType;
 use PAGEmachine\Searchable\Database\Connection;
 use PAGEmachine\Searchable\Indexer\PagesIndexer;
 use PAGEmachine\Searchable\Indexer\TcaIndexer;
 use PAGEmachine\Searchable\LinkBuilder\TypoLinkBuilder;
 use PAGEmachine\Searchable\Preview\NoPreviewRenderer;
+use PAGEmachine\Searchable\Queue\UpdateQueue;
 use PHPUnit\Framework\Attributes\Test;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Utility\ArrayUtility;
@@ -64,10 +66,12 @@ final class UpdateQueueTest extends FunctionalTestCase
     }
 
     #[Test]
-    public function insertDoesNotPolluteLasInsertIdOnDefaultConnection(): void
+    public function lastInsertIdIsNotPolluted(): void
     {
         $connection = $this->get(ConnectionPool::class)
             ->getConnectionForTable('tt_content');
+        $queueConnection = $this->get(ConnectionPool::class)
+            ->getConnectionByName(UpdateQueue::CONNECTION_NAME);
 
         $connection->insert('tt_content', [
             'pid' => 1,
@@ -75,6 +79,7 @@ final class UpdateQueueTest extends FunctionalTestCase
         ]);
 
         $reportedId = (int)$connection->lastInsertId();
+        $queueLastInsertId = (int)$queueConnection->lastInsertId();
 
         $actualUid = (int)$connection->createQueryBuilder()
             ->select('uid')
@@ -88,5 +93,19 @@ final class UpdateQueueTest extends FunctionalTestCase
             $reportedId,
             'lastInsertId() must return the tt_content uid, not a queue record uid'
         );
+
+        $updateQueryBuilder = $queueConnection->createQueryBuilder();
+
+        $updateResult = $updateQueryBuilder
+            ->select('*')
+            ->from('tx_searchable_update')
+            ->where(
+                $updateQueryBuilder->expr()->eq('uid', $updateQueryBuilder->createNamedParameter($queueLastInsertId, ParameterType::INTEGER)),
+            )
+            ->executeQuery()
+            ->fetchAssociative();
+
+        self::assertIsArray($updateResult, 'There should be a queue record from the content element insert');
+        self::assertSame($actualUid, (int)$updateResult['property_uid']);
     }
 }
